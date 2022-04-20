7f13832d652962e13cf2c89266d9fb41de3e5c6e
Luke-Tascone <99374613+Luke-Tascone@users.noreply.github.com>
2022-04-20 15:05:02 -0400

    Add files via upload

---
+++ Using-Enumeration.cpp
@@ -0,0 +1,53 @@
+// Lab 11B
+// Programmer : Luke Tascone
+
+#include <iostream>
+
+
+enum Menu {FRIES, HAMBURGER, HOTDOG, DRINK};
+void displayItemName (Menu);
+
+using namespace std;
+
+int main()
+{
+
+    int item;
+    double price = 0;
+    Menu menuItem;
+
+    int ordered[] = {0,0,0,0};
+    float prices[] = { 2.80, 4.99, 2.99, 1.20 };
+
+    for (menuItem = FRIES; menuItem <= DRINK; menuItem = static_cast<Menu>(menuItem + 1))
+    {
+        cout << "How many orders of ";
+        displayItemName(menuItem);
+        cout << ":  ";
+        cin >> ordered[menuItem];
+    }
+
+    for (menuItem = FRIES; menuItem <= DRINK; menuItem = static_cast<Menu>(menuItem + 1))
+    {
+        price += prices[menuItem] * ordered[menuItem];
+    }
+
+    cout << "Your order: " << endl;
+    cout << "Total $" << price << endl;
+
+    return 0;
+}
+
+void displayItemName (Menu d)
+{
+    switch(d)
+    {
+        case FRIES          : cout << "fries";
+                              break;
+        case HAMBURGER      : cout << "hamburgers";
+                              break;
+        case HOTDOG         : cout << "hotdogs";
+                              break;
+        case DRINK          : cout << "drinks";
+    }
+}
